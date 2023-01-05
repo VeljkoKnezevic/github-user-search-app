276e356a05b3303150f17d0d2c4a4902d924a630
Veljko <veljkoBitno@gmail.com>
2023-01-05 19:22:37 +0100

    Added conditional rendering

--- app.js
+++ app.js
@@ -1,49 +1,72 @@
-// const fetchData = async () => {
-//   const response = await fetch("https://api.github.com/users/octocat", {
-//     mode: "cors",
-//     headers: {
-//       Authorization: "Token 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",
-//     },
-//   });
-//   const data = await response.json();
+const fetchData = async () => {
+  const response = await fetch("https://api.github.com/users/octocat", {
+    mode: "cors",
+    headers: {
+      Authorization: "Token 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",
+    },
+  });
+  const data = await response.json();
 
-//   return data;
-// };
+  return data;
+};
 
-// const populateData = (() => {
-//   const user = document.querySelector(".user");
+const NotAvailableStyling = (element) => {
+  element.textContent = "Not Available";
+  element.parentElement.style.opacity = "0.5";
+};
 
-//   const profilePicture = user.children[0].children[0];
-//   const name = user.children[0].children[1];
-//   const at = user.children[0].children[2];
-//   const date = user.children[0].children[3];
+const populateData = (() => {
+  const profilePictureDesktop = document.querySelector(
+    ".user__picture__desktop"
+  );
+  const profilePicture = document.querySelector(".user__info__picture");
+  const name = document.querySelector(".user__info__name");
+  const at = document.querySelector(".user__info__at");
+  const date = document.querySelector(".user__info__date");
 
-//   const repos = user.children[2].children[0];
-//   const followers = user.children[2].children[1];
-//   const following = user.children[2].children[2];
+  const bio = document.querySelector(".user__bio");
 
-//   const location = user.children[3].children[0];
-//   const blog = user.children[3].children[1];
-//   const twitter = user.children[3].children[2];
-//   const company = user.children[3].children[3];
+  const repos = document.querySelector(".user__stats__repos");
+  const followers = document.querySelector(".user__stats__followers");
+  const following = document.querySelector(".user__stats__following");
 
-//   fetchData().then((response) => {
-//     profilePicture.src = response.avatar_url;
-//     name.textContent = response.name;
-//     at.textContent = `@${response.login}`;
-//     const responseDate = new Date(response.created_at).toDateString();
-//     date.textContent = `Joined
-//     ${responseDate.substring(8, 10)}
-//      ${responseDate.substring(4, 7)}
-//       ${responseDate.substring(10, 15)}`;
+  const location = document.querySelector(".user__links__location");
+  const blog = document.querySelector(".user__links__website");
+  const twitter = document.querySelector(".user__links__twitter");
+  const company = document.querySelector(".user__links__company");
 
-//     repos.textContent = response.public_repos;
-//     followers.textContent = response.followers;
-//     following.textContent = response.following;
+  fetchData().then((response) => {
+    profilePictureDesktop.src = response.avatar_url;
+    profilePicture.src = response.avatar_url;
+    name.textContent = response.name;
+    at.textContent = `@${response.login}`;
+    const responseDate = new Date(response.created_at).toDateString();
+    date.textContent = `Joined
+    ${responseDate.substring(8, 10)}
+     ${responseDate.substring(4, 7)}
+      ${responseDate.substring(10, 15)}`;
 
-//     location.textContent = response.location;
-//     blog.textContent = response.blog;
-//     twitter.textContent = response.twitter_username;
-//     company.textContent = response.company;
-//   });
-// })();
+    repos.textContent = response.public_repos;
+    followers.textContent = response.followers;
+    following.textContent = response.following;
+
+    if (response.bio) bio.textContent = response.bio;
+    else {
+      bio.textContent = "This profile has no bio.";
+      bio.style.opacity = "0.5";
+    }
+
+    if (response.location) location.textContent = response.location;
+    else NotAvailableStyling(location);
+
+    if (response.blog) blog.textContent = response.blog;
+    else NotAvailableStyling(blog);
+
+    if (response.twitter_username)
+      twitter.textContent = response.twitter_username;
+    else NotAvailableStyling(twitter);
+
+    if (response.company) company.textContent = response.company;
+    else NotAvailableStyling(company);
+  });
+})();
