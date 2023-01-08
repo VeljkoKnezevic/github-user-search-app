097566defac1ee14c30d691818803b65fd2e6a1a
Veljko <veljkoBitno@gmail.com>
2023-01-08 13:42:22 +0100

    Added dark mode functionality

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const fetchData = async () => {
-  const response = await fetch("https://api.github.com/users/octocat", {
-    mode: "cors",
-    headers: {
-      Authorization: "Token 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",
-    },
-  });
+const fetchData = async (value) => {
+  const response = await fetch(
+    `https://api.github.com/users/${value || "Octocat"}`,
+    {
+      mode: "cors",
+      headers: {
+        Authorization: "Token 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",
+      },
+    }
+  );
   const data = await response.json();
 
   return data;
@@ -15,7 +18,7 @@ const NotAvailableStyling = (element) => {
   element.parentElement.style.opacity = "0.5";
 };
 
-const populateData = (() => {
+const populateData = (value) => {
   const profilePictureDesktop = document.querySelector(
     ".user__picture__desktop"
   );
@@ -35,38 +38,81 @@ const populateData = (() => {
   const twitter = document.querySelector(".user__links__twitter");
   const company = document.querySelector(".user__links__company");
 
-  fetchData().then((response) => {
+  fetchData(value).then((response) => {
     profilePictureDesktop.src = response.avatar_url;
     profilePicture.src = response.avatar_url;
     name.textContent = response.name;
     at.textContent = `@${response.login}`;
     const responseDate = new Date(response.created_at).toDateString();
     date.textContent = `Joined
-    ${responseDate.substring(8, 10)}
-     ${responseDate.substring(4, 7)}
-      ${responseDate.substring(10, 15)}`;
+      ${responseDate.substring(8, 10)}
+       ${responseDate.substring(4, 7)}
+        ${responseDate.substring(10, 15)}`;
 
     repos.textContent = response.public_repos;
     followers.textContent = response.followers;
     following.textContent = response.following;
 
-    if (response.bio) bio.textContent = response.bio;
-    else {
-      bio.textContent = "This profile has no bio.";
+    if (response.bio) {
+      bio.textContent = response.bio;
+      bio.style.opacity = "1";
+    } else {
+      bio.textContent = "No bio available.";
       bio.style.opacity = "0.5";
     }
 
-    if (response.location) location.textContent = response.location;
-    else NotAvailableStyling(location);
+    if (response.location) {
+      location.textContent = response.location;
+      location.parentElement.style.opacity = "1";
+    } else NotAvailableStyling(location);
 
-    if (response.blog) blog.textContent = response.blog;
-    else NotAvailableStyling(blog);
+    if (response.blog) {
+      blog.textContent = response.blog;
+      blog.parentElement.style.opacity = "1";
+    } else NotAvailableStyling(blog);
 
-    if (response.twitter_username)
+    if (response.twitter_username) {
       twitter.textContent = response.twitter_username;
-    else NotAvailableStyling(twitter);
+      twitter.parentElement.style.opacity = "1";
+    } else NotAvailableStyling(twitter);
 
-    if (response.company) company.textContent = response.company;
-    else NotAvailableStyling(company);
+    if (response.company) {
+      company.textContent = response.company;
+      company.parentElement.style.opacity = "1";
+    } else NotAvailableStyling(company);
+  });
+};
+
+populateData();
+document.querySelector(".search").addEventListener("submit", (e) => {
+  e.preventDefault();
+  let input = e.target.elements[0];
+  populateData(input.value);
+
+  input.value = "";
+});
+
+const styles = () => {};
+
+const modeSwitcher = (() => {
+  const clickable = document.querySelector(".nav__clickable");
+  const image = document.querySelector(".nav__icon");
+
+  document.addEventListener("DOMContentLoaded", (e) => {
+    document.documentElement.setAttribute("data-theme", "light");
+  });
+
+  clickable.addEventListener("click", () => {
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+
+    let switchTheme = currentTheme === "dark" ? "light" : "dark";
+
+    document.documentElement.setAttribute("data-theme", switchTheme);
+
+    if (document.documentElement.getAttribute("data-theme") === "dark") {
+      image.src = "./assets/icon-sun.svg";
+    } else {
+      image.src = "./assets/icon-moon.svg";
+    }
   });
 })();
